7d73efb3b244c58050251e780c086bd42211db7b
fouadlee <fouadlee9@gmail.com>
2023-12-05 19:59:38 +0100

    adding basePath for images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_NEV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/manahil' : '',
     output: "export",
     distDir: "dist",
     images: {
